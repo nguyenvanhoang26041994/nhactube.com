5b9c8e6052f9fbecc67a8f76ec0505fdd67d8303
hvnguyen <hoang.van.nguyen@mgm-tp.com>
2019-11-20 13:56:19 +0700

    apply play BXH

--- src/containers/ChartMusic/ChartMusic.js
+++ src/containers/ChartMusic/ChartMusic.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useEffect } from 'react';
+import React, { useMemo, useEffect, useCallback } from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -7,6 +7,7 @@ import { Image, Icon, Button } from '../../components/core';
 import SongBar from '../../containers/SongBar';
 
 import { useBXHPlaylist } from './hooks';
+import { useGlobalPlayer } from '../../hooks';
 import { _take } from '../../utils';
 
 const Wrapper = styled.div`
@@ -40,17 +41,25 @@ const List = styled.ul`
 
 const ChartMusic = ({ className }) => {
   const { playlist, actions } = useBXHPlaylist();
+  const { playPlaylist } = useGlobalPlayer();
 
   useEffect(() => {
     actions.fetchBXH();
   }, []);
 
   const first15Music = useMemo(() => _take(playlist.songs, 15), [playlist.songs]);
+  const handlePlayXBH = useCallback(() => {
+    playPlaylist({
+      ...playlist,
+      songs: first15Music,
+    });
+  }, [playPlaylist, first15Music, playlist]);
+
   return (
     <Wrapper className={className}>
       <Handler className="w-1/3">
         <Avatar src={playlist.avatarUrl} />
-        <Button className="my-2">
+        <Button className="my-2" onClick={handlePlayXBH}>
           <Icon name="play" size="sm" className="mr-2" />
           Phát tất cả
         </Button>
